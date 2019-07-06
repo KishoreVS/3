87e4f6921b6fe99b2f13c675ed59b84bf33ac5b4
KishoreVS <33390009+KishoreVS@users.noreply.github.com>
2019-07-06 16:48:58 +0530

    Add files via upload

---
+++ Ex1.java
@@ -0,0 +1,40 @@
+package lab3;
+
+import java.util.Scanner;
+
+public class Ex1 {
+	static int temp;
+	int a[];
+	int getSecondSmallest(int a[])
+	{
+    for (int i=0;i<a.length;i++) 
+    {
+        for (int j=i+1;j<a.length;j++) 
+        {
+            if (a[i] > a[j]) 
+            {
+                temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+    }
+		return a[1];
+	}
+	public static void main(String[] args) {	
+	Scanner s=new Scanner(System.in);
+	System.out.println("Enter the no of elements in the array");
+	int n=s.nextInt();
+	int a[] = new int[n];//3
+    System.out.println("Enter all the elements:");
+	 for (int i=0;i<a.length;i++) 
+	    {
+	        a[i] = s.nextInt();
+	      
+	    }
+	 Ex1 e=new Ex1();
+		e.getSecondSmallest(a);
+		System.out.println("Second Smallest in Array= "+a[1]);
+    s.close();
+}
+}
---
+++ Ex2.java
@@ -0,0 +1,74 @@
+package lab3;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Ex2 {
+	 public static String sortString(String inputString) 
+	    { 
+	        // convert input string to char array 
+	        char tempArray[] = inputString.toCharArray(); 
+	          
+	        // sort tempArray 
+	        Arrays.sort(tempArray); 
+	          
+	        // return new sorted string 
+	        return new String(tempArray); 
+	    } 
+	      
+
+	public static void main(String[] args) 
+    {
+       String str;
+       Scanner sc=new Scanner(System.in);
+       System.out.println("Enter a string:");
+       str=sc.nextLine();
+       str=sortString(str);
+       int n=str.length();
+       String str2="";
+       String str3="";
+       String str4="";
+       String str5="";
+       String s1=null,s2=null;
+       String s3=null,s4=null;
+       
+       if(n%2==0)
+       {
+    	   for(int i=0;i<n;i++)
+    	   {
+    		   if(i<n/2)
+    		   {
+    			   str2+=str.charAt(i);
+    				s1=str2.toUpperCase();	      
+    		   }
+    		   else
+    		   {
+    			   str3+=str.charAt(i);
+    			   s2=str3.toLowerCase();
+    		   }
+    	   }
+    	   System.out.println(s1);
+    	   System.out.println(s2);
+       }
+       else
+       {
+    	   for(int i=0;i<n;i++)
+    	   {
+    		   if(i<n/2+1)
+    		   {
+    			   str4+=str.charAt(i);
+    		   s3=str4.toUpperCase();
+    		   }
+    		   else
+    		   {
+    			   str5+=str.charAt(i);
+    			   s4=str5.toLowerCase();
+    		   }
+    	   }
+    		   System.out.println(s3);
+    		   System.out.println(s4);
+
+       }
+       sc.close();
+}
+}
